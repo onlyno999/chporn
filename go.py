380e12ad84eeb6169412eb93b4ca370bb32412a9
VeG <62206240+vveg26@users.noreply.github.com>
2022-10-16 18:42:34 +0800

    Update go.py

--- go.py
+++ go.py
@@ -44,7 +44,7 @@ def get_url(url):
 
 
 
-fi=open('adult.txt','r')
+fi=open('adult.txt','wr')
 headers = {
         'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
         }  # 构造请求头
